116b3dde2225f7b8b55ca90d1bd4a9b6c6c294a6
Anselm <abairdsmith@gmail.com>
2024-10-18 12:19:05 +0200

    Revamped code, works with both entry method: - Enter by purchasing count times a set amount of shares, - Enter by reaching the desired cash allocation target in steps of target / count. So by the end of the purchase period, you end up with target cash and (1-target) stock.

--- entry.py
+++ entry.py
@@ -38,10 +38,12 @@ parser.add_argument('--cash', type=int, default=10000,
                     help='Initial amount of cash to work with.')
 parser.add_argument('-v', '--verbose', action='count', default=0,
                     help='Chat some as we procceed.')
-parser.add_argument('--count', type=int, default=30,
+parser.add_argument('--count', type=int, default=12,
                     help='Number of purchases to make.')
-parser.add_argument('--every', default=10,
-                    help='Frequency - in business days - of purchases.')
+parser.add_argument('--target', type=int, default=0.2,
+                    help='Target cash allocation by end of purchase period.')
+parser.add_argument('--freq', default='ME',
+                    help='Frequency of purchases, e.g. "ME", "W" ...')
 parser.add_argument('--from', type=datetime.date.fromisoformat, default=None,
                     dest='from_datetime',
                     help='Restrict analysis to data later than this date (YYYY-MM-DD)')
@@ -50,6 +52,8 @@ parser.add_argument('--till', type=datetime.date.fromisoformat, default=None,
                     help='Restrict analysis to data earlier than this date (YYYY-MM-DD)')
 
 args = parser.parse_args()
+args.freq = 'W-MON'
+args.count = 52
 
 def verbose(level: int, msg: str):
     if args.verbose >= level:
@@ -58,7 +62,7 @@ def verbose(level: int, msg: str):
 def exit(msg: str):
     sys.exit(msg)
 
-def yearly_returns(data: pd.DataFrame, start_value: float, end_value: float):
+def annual_returns(data: pd.DataFrame, start_value: float, end_value: float):
     days = (data.index[-1] - data.index[0]).days
     gain = 1.0 + (end_value - start_value) / start_value
     per_day = math.pow(gain, 1 / days)
@@ -66,29 +70,52 @@ def yearly_returns(data: pd.DataFrame, start_value: float, end_value: float):
 
 # Enters the market with initial_cash.
 def enter(data: pd.DataFrame, 
-          initial_cash: float, 
-          count: int = args.count,
-          every: int = args.every):
-    amount = initial_cash / count
-    cash = initial_cash
-    stock = 0
-    for ts, row in data.iloc[::every].iterrows():
-        quantity = math.floor(min(amount, cash) / row.Close)
-        if quantity > 0:
-            verbose(2, f"{ts} => BUY {quantity} @ {row.Close:.2f}")
-            stock += quantity
-            cash -= quantity * row.Close
-        count -= 1
-        if count <= 0:
-            break
-    # (value, position, cash)
-    return stock * data.iloc[-1].Close, stock, cash
+          initial_cash: float, target: float,
+          freq: str = args.freq, count: int = args.count):
+    # Enter the market over a year, buy every week
+    cash_reserve = target * initial_cash
+    initial_cash -= cash_reserve
+    amount_step = initial_cash / count
+    out = data.copy()
+    out['Purchase'] = False
+    out.loc[out.asfreq(freq).dropna().index, 'Purchase'] = True
+    
+    # Setup initial values
+    price = out.iloc[0].Close
+    position = math.floor(amount_step / price)
+    cash = initial_cash - position * price
+    count -= 1
+    class State:
+        def __init__(self):
+            self.position = [ position ]
+            self.cash = [ cash ]
+            self.value = [ position * price + cash ]
+            self.index = [ data.index[0] ]
+    state = State()
+    for ts, row in out[1:].iterrows():
+        if row.Purchase and count > 0:
+            buy = math.floor(min(amount_step, cash) / row.Close)
+            if buy > 0:
+                position += buy
+                cash -= buy * row.Close
+                verbose(level=1, msg=f"[{count}] {ts} BUY {buy} => ${cash:,.2f} {position}/${position * row.Close:,.2f} ${cash + position * row.Close:,.2f}")
+            count -= 1
+        state.position.append(position)
+        state.cash.append(cash + cash_reserve)
+        state.value.append(cash + position * row.Close)
+        state.index.append(ts)
+    return data.copy().join([
+        pd.Series(state.position, name='Position', index=state.index), 
+        pd.Series(state.cash, name='Cash', index=state.index), 
+        pd.Series(state.value, name='Value', index=state.index)
+    ])
 
 def enter2(data: pd.DataFrame, 
-            initial_cash: float,
-            freq = 'ME', count = 12):
+           initial_cash: float,
+           target: float,
+           freq: str = args.freq, count: int = args.count):
     # Enter the market over a year, buy every week
-    final_target = (1 - 0.01)
+    final_target = (1 - 0.2)
     target_step = final_target / count
     out = data.copy()
     out['Rebalance'] = False
@@ -128,15 +155,13 @@ def enter2(data: pd.DataFrame,
         pd.Series(state.value, name='Value', index=state.index)
     ])
 
-def plot_by_every(data: pd.DataFrame):
-    scope = range(1, 10)
-    def value(every: int):
-        value, _, _ = enter(data, args.cash, count=25, every=every)
-        yoy = yearly_returns(data, args.cash, value)
-        verbose(1, f"Freq {every} over {25*every} days => ${value:,.2f} YoY {yoy:.2f}")
-        return yoy
-    plt.plot(scope, [value(every) for every in scope])
-    plt.show()
+def display(out: pd.DataFrame):
+    row = out.iloc[-1]
+    print(f"{out.index[-1]} => \
+        Cash ${row.Cash:,.2f} \
+        Position {row.Position:,.2f} \
+        Value: ${row.Value:,.2f} \
+        YoY {annual_returns(out, args.cash, row.Value):.2f}%")
 
 def main():
     # Fetch and cut the data according to the command line.
@@ -151,11 +176,10 @@ def main():
         exit(f"No data available for {args.symbol}, check your spelling.")
     # Do as requested.
     verbose(1, f"Using {args.symbol} data from {data.index[0]} till {data.index[-1]}")
-    (value, position, cash) = enter(data, args.cash, count = 12, every = 30)
-    print(f"Bought {position} shares. ${cash:.2f} left => ${value:,.2f}")
-#    plot_by_every(data)
-    out = enter2(data, args.cash)
-    print(out.tail(1))
+    out = enter(data, args.cash, args.target)    
+    display(out)
+    out = enter2(data, args.cash, args.target)
+    display(out)
 
 if __name__ == "__main__":
     main()
