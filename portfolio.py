670524381bd2a06dcce688bd324072df18387684
Anselm <abairdsmith@gmail.com>
2024-10-24 11:46:19 +0200

    Multi ticker rebalance and corresponding tests.

--- portfolio.py
+++ portfolio.py
@@ -1,36 +1,102 @@
-from typing import Dict
+from typing import Dict, Self
+import math
 
 class Portfolio:
-    stocks: Dict[str, int]
-    cash: float
+    CASH_SYMBOL = '$$CASH'
+    
+    _positions: Dict[str, int]
+    _cash: float
 
     def __init__(self, cash: float = 100000.0):
-        self.cash = cash
-        self.stocks = { }
+        self._cash = cash
+        self._positions = { }
+        self.alloc = { self.CASH_SYMBOL: 1.0 }
 
     @staticmethod
-    def norm(symbol: str):
+    def norm(symbol: str) -> str :
         return symbol.upper()
     
-    def buy(self, symbol, quantity: int, price: float):
+    def buy(self, symbol, quantity: int, price: float) -> int:
         symbol = Portfolio.norm(symbol)
-        self.stocks[symbol] = self.stocks.get(symbol, 0) + quantity
-        self.cash -= (quantity * price)
-        assert(self.cash >= 0.0)
-        return self.stocks[symbol]
+        self._positions[symbol] = self._positions.get(symbol, 0) + quantity
+        self._cash -= (quantity * price)
+        assert(self._cash >= 0.0)
+        return self._positions[symbol]
 
-    def sell(self, symbol, quantity, price):
+    def sell(self, symbol, quantity, price) -> int:
         symbol = Portfolio.norm(symbol)
-        assert(quantity <= self.stocks.get(symbol, 0))
-        self.stocks[symbol] -= quantity
-        if self.stocks[symbol] == 0:
-            del self.stocks[symbol]
-        self.cash += (quantity * price)
+        assert(quantity <= self._positions.get(symbol, 0))
+        self._positions[symbol] -= quantity
+        self._cash += (quantity * price)
+        if self._positions[symbol] == 0:
+            del self._positions[symbol]
+            return 0
+        else:
+            return self._positions[symbol]
 
-    def position(self, symbol: str):
+    def position(self, symbol: str) -> float:
         symbol = Portfolio.norm(symbol)
-        return self.stocks.get(symbol, 0)
+        return self._positions.get(symbol, 0)
+    
+    def cash(self) -> float:
+        return self._cash
+    
+    def set_allocation(self, alloc: Dict[str, float]) -> Self:
+        if self.CASH_SYMBOL not in alloc:
+            alloc[self.CASH_SYMBOL] = 1.0 - sum(alloc.values())
+        assert(math.isclose(1.0, sum(alloc.values())))
+        self.alloc = { self.norm(ticker): target for ticker, target in alloc.items() }
+        return self
+        
+    def get_target_allocation(self, symbol: str) -> float:
+        return self.alloc.get(self.norm(symbol), 0)  
+    
+    def balance(self, prices: Dict[str, float]) -> Self:
+        value = self._cash + sum(self.position(ticker) * prices[ticker] for ticker in self._positions)
+        alloc = { ticker: 0.0 for ticker in self._positions.keys() }
+        for ticker in self.alloc.keys():
+            alloc[ticker] = value * self.alloc[ticker]
+        print(alloc)
+        for ticker, target in alloc.items():
+            if ticker == self.CASH_SYMBOL: 
+                continue
+            price = prices[ticker]
+            order = int(math.floor(target / price) - self.position(ticker))
+            if order > 0:
+                self.buy(ticker, order, price)
+            elif order < 0:
+                self.sell(ticker, - order, price)
+            print(f"{"B" if order > 0 else "S"} {ticker}: {order} @ {price}")
+        return self
+    
+pf = Portfolio()
+pf.set_allocation({
+    'VTI': 0.5,
+    'GOOG': 0.2
+})
+pf.balance({
+    'VTI': 100.0,
+    'GOOG': 20.0,
+})
 
+
+pf.set_allocation({
+    'VTI': 0.5,
+})
+pf.balance({
+    'VTI': 100.0,
+    'GOOG': 20.0,
+})
+
+
+pf.set_allocation({
+    'VTI': 0.5,
+    'GOOG': 0.2
+})
+pf.balance({
+    'VTI': 100.0,
+    'GOOG': 20.0,
+})
 
     
     
--- test_portfolio.py
+++ test_portfolio.py
@@ -6,13 +6,13 @@ class TestPortfolio(unittest.TestCase):
 
     def test_default_cash(self):
         p = Portfolio()
-        self.assertEqual(p.cash, 100000.0)
+        self.assertEqual(p._cash, 100000.0)
 
     def test_buy(self):
         p = Portfolio()
-        self.assertEqual(p.cash, 100000.0)
+        self.assertEqual(p._cash, 100000.0)
         p.buy('vti', 10, 100.0)
-        self.assertEqual(p.cash, 99000.0)
+        self.assertEqual(p._cash, 99000.0)
         self.assertEqual(p.position('vti'), 10)
 
     def test_sell_none(self):
@@ -26,14 +26,52 @@ class TestPortfolio(unittest.TestCase):
 
     def test_sell(self):
         p = Portfolio()
-        self.assertEqual(p.cash, 100000.0)
+        self.assertEqual(p._cash, 100000.0)
         p.buy('vti', 10, 100.0)
-        self.assertEqual(p.cash, 99000.0)
+        self.assertEqual(p._cash, 99000.0)
         self.assertEqual(p.position('vti'), 10)
         p.sell('vti', 10, 100.0)
-        self.assertEqual(p.cash, 100000.0)
-
+        self.assertEqual(p._cash, 100000.0)
         
+    def test_set_allocation(self):
+        p = Portfolio()
+        self.assertAlmostEqual(1.0, p.get_target_allocation(p.CASH_SYMBOL))
+        p.set_allocation({
+            'VTI': 0.8
+        })
+        self.assertAlmostEqual(0.2, p.get_target_allocation(Portfolio.CASH_SYMBOL))
+
+    def test_rebalance(self):
+        prices = {
+            'VTI': 100.0,
+            'GOOG': 20.0,
+        }
+        p = Portfolio()
+        p.set_allocation({
+            'VTI': 0.5,
+            'GOOG': 0.2
+        })
+        p.balance(prices)
+        self.assertEqual(500, p.position('VTI'))
+        self.assertEqual(1000, p.position('GOOG'))
+        self.assertAlmostEqual(30000.0, p.cash())
+        # Change allocation and rebalance.
+        p.set_allocation({
+            'VTI': 0.5,
+        })
+        p.balance(prices)
+        self.assertEqual(500, p.position('VTI'))
+        self.assertEqual(0, p.position('GOOG'))
+        self.assertAlmostEqual(50000.0, p.cash())
+        # And back again:
+        p.set_allocation({
+            'VTI': 0.5,
+            'GOOG': 0.2
+        })
+        p.balance(prices)
+        self.assertEqual(500, p.position('VTI'))
+        self.assertEqual(1000, p.position('GOOG'))
+        self.assertAlmostEqual(30000.0, p.cash())
 
 if __name__ == '__main__':
     unittest.main()
